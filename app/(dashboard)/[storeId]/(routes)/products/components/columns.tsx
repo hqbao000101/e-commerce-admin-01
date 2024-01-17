197a75da3fed02295ccd40872fc3753555a3327a
Huỳnh Quang Bảo <hbao592000@gmail.com>
2024-01-17 11:56:36 +0700

    feat: product-pages-fe

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, Settings } from "lucide-react";
+import { CellAction } from "./cell-actions";
+
+export type ProductColumn = {
+  id: string;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  isArchived: boolean;
+  category: string;
+  size: string;
+  color: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="w-4 h-4 ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="pl-4">{row.original.name}</div>,
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="w-4 h-4 ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="pl-4">{row.original.price}</div>,
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "color",
+    header: "Color",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        <div
+          className="w-6 h-6 border rounded-full"
+          style={{ backgroundColor: row.original.color }}
+        />
+        {row.original.color}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Datetime",
+  },
+  {
+    id: "actions",
+    header: () => <Settings className="w-4 h-4 mx-auto" />,
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
